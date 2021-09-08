17368e5792e864f6c2713b88ed45fc8519e31727
Enesİnanç <enes.inanc@arcelik.com>
2021-09-08 12:34:02 +0300

    First Commit. Siamese Network Example

---
+++ Keras_ex2_TrainSiameseNetwork.py
@@ -0,0 +1,163 @@
+# import the necessary packages
+import os
+# import the necessary packages
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Lambda
+from tensorflow.keras.datasets import mnist
+
+# import the necessary packages
+import tensorflow.keras.backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+
+# specify the shape of the inputs for our network
+IMG_SHAPE = (28, 28, 1)
+# specify the batch size and number of epochs
+BATCH_SIZE = 64
+EPOCHS = 5
+
+# define the path to the base output directory
+BASE_OUTPUT = "output"
+# use the base output path to derive the path to the serialized
+# model along with training history plot
+MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "siamese_model"])
+PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
+
+
+def build_siamese_model(inputShape, embeddingDim=48):
+    # specify the inputs for the feature extractor network
+    inputs = Input(inputShape)
+    # define the first set of CONV => RELU => POOL => DROPOUT layers
+    x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.3)(x)
+    # second set of CONV => RELU => POOL => DROPOUT layers
+    x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
+    x = MaxPooling2D(pool_size=2)(x)
+    x = Dropout(0.3)(x)
+
+    # prepare the final outputs
+    pooledOutput = GlobalAveragePooling2D()(x)
+    outputs = Dense(embeddingDim)(pooledOutput)
+    # build the model
+    model = Model(inputs, outputs)
+    # return the model to the calling function
+    return model
+
+
+def make_pairs(images, labels):
+    # initialize two empty lists to hold the (image, image) pairs and
+    # labels to indicate if a pair is positive or negative
+    pairImages = []
+    pairLabels = []
+    # calculate the total number of classes present in the dataset
+    # and then build a list of indexes for each class label that
+    # provides the indexes for all examples with a given label
+    numClasses = len(np.unique(labels))
+    idx = [np.where(labels == i)[0] for i in range(0, numClasses)]
+    # loop over all images
+    for idxA in range(len(images)):
+        # grab the current image and label belonging to the current
+        # iteration
+        currentImage = images[idxA]
+        label = labels[idxA]
+        # randomly pick an image that belongs to the *same* class
+        # label
+        idxB = np.random.choice(idx[label])
+        posImage = images[idxB]
+        # prepare a positive pair and update the images and labels
+        # lists, respectively
+        pairImages.append([currentImage, posImage])
+        pairLabels.append([1])
+        # grab the indices for each of the class labels *not* equal to
+        # the current label and randomly pick an image corresponding
+        # to a label *not* equal to the current label
+        negIdx = np.where(labels != label)[0]
+        negImage = images[np.random.choice(negIdx)]
+        # prepare a negative pair of images and update our lists
+        pairImages.append([currentImage, negImage])
+        pairLabels.append([0])
+    # return a 2-tuple of our image pairs and labels
+    return (np.array(pairImages), np.array(pairLabels))
+
+
+def euclidean_distance(vectors):
+    # unpack the vectors into separate lists
+    (featsA, featsB) = vectors
+    # compute the sum of squared distances between the vectors
+    sumSquared = K.sum(K.square(featsA - featsB), axis=1, keepdims=True)
+    # return the euclidean distance between the vectors
+    return K.sqrt(K.maximum(sumSquared, K.epsilon()))
+
+
+def plot_training(H, plotPath):
+    # construct a plot that plots and saves the training history
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(H.history["loss"], label="train_loss")
+    plt.plot(H.history["val_loss"], label="val_loss")
+    plt.plot(H.history["accuracy"], label="train_acc")
+    plt.plot(H.history["val_accuracy"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig(plotPath)
+
+
+# load MNIST dataset and scale the pixel values to the range of [0, 1]
+print("[INFO] loading MNIST dataset...")
+(trainX, trainY), (testX, testY) = mnist.load_data()
+print("------- mnist dataset info -------",
+      "\ntrainX.shape:", trainX.shape, "trainY.shape:", trainY.shape,
+      "\ntestX.shape:", testX.shape, "testY.shape:", testY.shape)
+
+trainX = trainX / 255.0
+testX = testX / 255.0
+# add a channel dimension to the images
+trainX = np.expand_dims(trainX, axis=-1)
+testX = np.expand_dims(testX, axis=-1)
+print("------- mnist dataset info after extend dim -------",
+      "\ntrainX.shape:", trainX.shape, "trainY.shape:", trainY.shape,
+      "\ntestX.shape:", testX.shape, "testY.shape:", testY.shape)
+# prepare the positive and negative pairs
+print("[INFO] preparing positive and negative pairs...")
+(pairTrain, labelTrain) = make_pairs(trainX, trainY)
+(pairTest, labelTest) = make_pairs(testX, testY)
+
+# configure the siamese network
+print("[INFO] building siamese network...")
+imgA = Input(shape=IMG_SHAPE)
+imgB = Input(shape=IMG_SHAPE)
+featureExtractor = build_siamese_model(IMG_SHAPE)
+featsA = featureExtractor(imgA)
+featsB = featureExtractor(imgB)
+
+# finally, construct the siamese network
+distance = Lambda(euclidean_distance)([featsA, featsB])
+outputs = Dense(1, activation="sigmoid")(distance)
+model = Model(inputs=[imgA, imgB], outputs=outputs)
+
+# # compile the model
+# print("[INFO] compiling model...")
+# model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+# # train the model
+# print("[INFO] training model...")
+# history = model.fit(
+#     [pairTrain[:, 0], pairTrain[:, 1]], labelTrain[:],
+#     validation_data=([pairTest[:, 0], pairTest[:, 1]], labelTest[:]),
+#     batch_size=BATCH_SIZE,
+#     epochs=EPOCHS)
+#
+# # serialize the model to disk
+# print("[INFO] saving siamese model...")
+# model.save(MODEL_PATH)
+# # plot the training history
+# print("[INFO] plotting training history...")
+# plot_training(history, PLOT_PATH)
